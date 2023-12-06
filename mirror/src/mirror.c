eccde3637a33bbd9c31937bc0d3521851e248ad4
NataliaLeveta <61879991+NataliaLeveta@users.noreply.github.com>
2023-12-06 22:16:24 +0200

    Update mirror.c

--- mirror/src/mirror.c
+++ mirror/src/mirror.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <math.h>
 
-int prime(long long n){
+int prime(long long n){ // function that checks the prime numbres
     long long divisor;
     int is_prime = 1;
-	if((n != 2 && n % 2 == 0) || (n != 3 && n % 3 == 0)){
+	if((n != 2 && n % 2 == 0) || (n != 3 && n % 3 == 0)){ // checkes if the numbres can be divided by 2 or 3
 		is_prime = 0;
 	}
 	else {
-		for (divisor = 5; divisor * divisor <= n ; divisor += 6){
+		for (divisor = 5; divisor * divisor <= n ; divisor += 6){ // checkes if the numbres can be divided by any other number
 			if(n % divisor == 0 || n % (divisor + 2) == 0){
 				is_prime = 0;
 				break;
@@ -19,7 +19,7 @@ int prime(long long n){
     return is_prime;
 }
 
-long long flip(long long n){
+long long flip(long long n){ // function that reverses the digits of the number
     long long remain, rever = 0;
     while (n != 0){
         remain = n % 10;
@@ -37,13 +37,13 @@ int main (int argc, char *argv[]){ // takes arguments from the command line
         if (low <= high && low >= 1 && high <= 1000000000000000){
             long long sum = 0;
             long long x = sqrt(low);
-            while (x*x <= high ){
+            while (x*x <= high ){ // loop through the numbers in the given range
                 if (prime(x)){
                     long long n1 = x*x;
                     long long n2 = flip(n1);
                     if (n1 != n2){
                         long long temp = sqrt(n2);
-                        if (temp * temp == n2){
+                        if (temp * temp == n2){ // check if root of n2 is integer
                             if (prime(temp)){
                                 sum += n1;
                             }
@@ -52,10 +52,9 @@ int main (int argc, char *argv[]){ // takes arguments from the command line
                 }
                 x++;
             }
-            printf("%lld \n", sum);
+            printf("%lld \n", sum); // print the sum of the numbers
         }
         else 
             return 1;   
     }
 }
-
